74366ac349a080f15c742dc50b22e11b21971062
Matheus <matheus.uspeu@gmail.com>
2021-08-19 19:01:24 -0300

    projeto calculadora react

---
+++ calculadora-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from './main/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculadora</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
